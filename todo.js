ea30680c5b0d0821328cb6f43a1289071377b106
CrUx08 <princeimbabazi08@gmail.com>
2025-06-11 18:42:38 +0200

    updated to do

--- todo.js
+++ todo.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+   const input = document.querySelector('#Taskinput');
+   const submit = document.querySelector('#submit');
+   submit.disabled = true;
+   input.addEventListener('input',()=>{
+    if (input.value.trim() === ''){
+        submit.disabled = true;
+       }
+       else {
+        submit.disabled = false;
+       }
+   })
     document.querySelector('form').onsubmit = (event) => {
         event.preventDefault();
         const task = document.querySelector('#Taskinput').value;
@@ -12,9 +23,8 @@ document.addEventListener('DOMContentLoaded', function() {
         tasktime.innerHTML = time;
         
         document.querySelector('#tasklist').append(tasks)
-        document.querySelector('#tasklist').append(tasktime)
         document.querySelector('#Taskinput').value = '';
         document.querySelector('#date').value = '';
-
+        
     };
 });
